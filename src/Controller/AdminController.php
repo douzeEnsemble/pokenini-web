42fe29c950070d9bdffdcb41e552a5076f6ba419
rlerognon <renaud.lerognon@fulll.fr>
2022-09-24 12:20:01 +0200

    Add Admin page to update data

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+#[Route('/istrateur')]
+class AdminController extends AbstractController
+{
+    #[Route('/', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('Admin/index.html.twig');
+    }
+}
