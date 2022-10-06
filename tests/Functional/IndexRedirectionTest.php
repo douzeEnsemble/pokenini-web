0ee984fa47d20eaa539bcd31b301c31368789033
rlerognon <renaud.lerognon@fulll.fr>
2022-10-06 10:21:34 +0200

    Add redirection to default locale

---
+++ tests/Functional/IndexRedirectionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexRedirectionTest extends WebTestCase
+{
+    public function testRedirection(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertResponseStatusCodeSame(301);
+        $crawler = $client->followRedirect();
+
+        $this->assertEquals('http://localhost/fr/', $crawler->getUri());
+    }
+}
