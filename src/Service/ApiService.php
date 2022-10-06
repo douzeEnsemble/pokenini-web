020fab25c3e405000a33cb4bad632b28b8163ac7
rlerognon <renaud.lerognon@fulll.fr>
2022-10-06 15:08:37 +0200

    Add declare strict types and api service method `updateAlbum`

--- src/Service/ApiService.php
+++ src/Service/ApiService.php
@@ -1,13 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Service;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ApiService
 {
-
     public function __construct(
         private readonly HttpClientInterface $client,
         private readonly string $appApiUrl,
@@ -20,6 +22,7 @@ class ApiService
      */
     public function getDexes(): array
     {
+        /** @var string $json */
         $json = $this->cache->get('dexes', function () {
             $response = $this->client->request(
                 'GET',
@@ -44,6 +47,7 @@ class ApiService
      */
     public function getPokedex(string $dexSlug): array
     {
+        /** @var string $json */
         $json = $this->cache->get("album_$dexSlug", function () use ($dexSlug) {
             $response = $this->client->request(
                 'GET',
@@ -63,6 +67,7 @@ class ApiService
      */
     public function getCatchStates(): array
     {
+        /** @var string $json */
         $json = $this->cache->get("catch_states", function () {
             $response = $this->client->request(
                 'GET',
@@ -80,5 +85,24 @@ class ApiService
 
         /** @var string[][] */
         return json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+    }
+
+    public function modifyAlbum(
+        string $method,
+        string $dexSlug,
+        string $pokemonSlug,
+        string $catchStateSlug
+    ): void {
+        if (!in_array($method, [Request::METHOD_PATCH, Request::METHOD_PUT])) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->client->request(
+            $method,
+            "{$this->appApiUrl}/album/$dexSlug/$pokemonSlug",
+            [
+                'body' => $catchStateSlug,
+            ]
+        );
     }
 }
