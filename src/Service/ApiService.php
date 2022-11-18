502595fe72a3228455c9b36439f4487d7ca6edc8
Renaud Douze <renauddouze@gmail.com>
2022-11-18 23:32:57 +0100

    Fix 404 from Api on album (bis)

--- src/Service/ApiService.php
+++ src/Service/ApiService.php
@@ -69,7 +69,7 @@ class ApiService
         $json = $this->cache->get($key, function () use ($dexSlug, $trainerId) {
             $response = $this->client->request(
                 'GET',
-                "{$this->appApiUrl}/$trainerId/album/$dexSlug"
+                "{$this->appApiUrl}/album/$trainerId/$dexSlug"
             );
 
             /** @var string */
