4c6fe245658a0c7128684c2e3e6ae80bba4362e2
Renaud Douze <renauddouze@gmail.com>
2022-11-18 23:20:36 +0100

    Revert "Fix 404 from Api on album"
    This reverts commit bfc09ad99e2b603e292f8880d41e55b6e07eb0a3.

--- src/Service/ApiService.php
+++ src/Service/ApiService.php
@@ -69,7 +69,7 @@ class ApiService
         $json = $this->cache->get($key, function () use ($dexSlug, $trainerId) {
             $response = $this->client->request(
                 'GET',
-                "{$this->appApiUrl}/dex/$trainerId/$dexSlug"
+                "{$this->appApiUrl}/$trainerId/album/$dexSlug"
             );
 
             /** @var string */
