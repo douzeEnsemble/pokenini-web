4c52cead61eb4a3ebbde6ccfb1e5465c5359d042
Renaud Douze <renaud.douze@gmail.com>
2023-05-19 10:31:12 +0200

    Fix divide by zero with virgin dex

--- tests/Functional/Album/Display/CommonTest.php
+++ tests/Functional/Album/Display/CommonTest.php
@@ -55,6 +55,19 @@ class CommonTest extends WebTestCase
         $this->assertShiny($client);
     }
 
+    /**
+     * Brand new dex has an issue with a division by zero
+     */
+    public function testListVirgin(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/fr/album/virgin?t=7b52009b64fd0a2a49e6d8a939753077792b0554');
+
+        $this->assertAlbum($client);
+        $this->assertViriginStatistics($client);
+    }
+
     private function assertAlbum(KernelBrowser $client): void
     {
         $this->assertResponseIsSuccessful();
@@ -208,6 +221,94 @@ class CommonTest extends WebTestCase
         );
         $this->assertStringContainsString(
             '/album/demolite',
+            (string) $crawler->filter('table#report tr.catch-state-total a')->attr('href')
+        );
+    }
+
+    private function assertViriginStatistics(KernelBrowser $client): void
+    {
+        $crawler = $client->getCrawler();
+
+        $this->assertCountFilter($crawler, 1, 'h2#stats');
+
+        $this->assertCountFilter($crawler, 1, '.progress');
+        $this->assertCountFilter($crawler, 6, '.progress-bar');
+
+        $this->assertEquals(
+            '0%',
+            $crawler->filter('.progress-bar.catch-state-no')->text()
+        );
+        $this->assertEmpty(
+            $crawler->filter('.progress-bar.catch-state-toevolve')->text()
+        );
+        $this->assertEmpty(
+            $crawler->filter('.progress-bar.catch-state-tobreed')->text()
+        );
+        $this->assertEmpty(
+            $crawler->filter('.progress-bar.catch-state-totransfer')->text()
+        );
+        $this->assertEquals(
+            '0%',
+            $crawler->filter('.progress-bar.catch-state-yes')->text()
+        );
+
+        $this->assertCountFilter($crawler, 1, 'table#report');
+        $this->assertCountFilter($crawler, 7, 'table#report tr');
+
+        $this->assertCountFilter($crawler, 1, 'table#report tr.catch-state-no');
+        $this->assertCountFilter($crawler, 1, 'table#report tr.catch-state-toevolve');
+        $this->assertCountFilter($crawler, 1, 'table#report tr.catch-state-tobreed');
+        $this->assertCountFilter($crawler, 1, 'table#report tr.catch-state-totransfer');
+        $this->assertCountFilter($crawler, 1, 'table#report tr.catch-state-yes');
+        $this->assertCountFilter($crawler, 1, 'table#report tr.catch-state-total');
+
+        $this->assertEquals(
+            '0',
+            $crawler->filter('table#report tr.catch-state-no td')->text()
+        );
+        $this->assertEquals(
+            '0',
+            $crawler->filter('table#report tr.catch-state-toevolve td')->text()
+        );
+        $this->assertEquals(
+            '0',
+            $crawler->filter('table#report tr.catch-state-tobreed td')->text()
+        );
+        $this->assertEquals(
+            '0',
+            $crawler->filter('table#report tr.catch-state-totransfer td')->text()
+        );
+        $this->assertEquals(
+            '0',
+            $crawler->filter('table#report tr.catch-state-yes td')->text()
+        );
+        $this->assertEquals(
+            '0',
+            $crawler->filter('table#report tr.catch-state-total td')->text()
+        );
+
+        $this->assertStringContainsString(
+            '/album/virgin?t=7b52009b64fd0a2a49e6d8a939753077792b0554&cs=no',
+            (string) $crawler->filter('table#report tr.catch-state-no a')->attr('href')
+        );
+        $this->assertStringContainsString(
+            '/album/virgin?t=7b52009b64fd0a2a49e6d8a939753077792b0554&cs=toevolve',
+            (string) $crawler->filter('table#report tr.catch-state-toevolve a')->attr('href')
+        );
+        $this->assertStringContainsString(
+            '/album/virgin?t=7b52009b64fd0a2a49e6d8a939753077792b0554&cs=tobreed',
+            (string) $crawler->filter('table#report tr.catch-state-tobreed a')->attr('href')
+        );
+        $this->assertStringContainsString(
+            '/album/virgin?t=7b52009b64fd0a2a49e6d8a939753077792b0554&cs=totransfer',
+            (string) $crawler->filter('table#report tr.catch-state-totransfer a')->attr('href')
+        );
+        $this->assertStringContainsString(
+            '/album/virgin?t=7b52009b64fd0a2a49e6d8a939753077792b0554&cs=yes',
+            (string) $crawler->filter('table#report tr.catch-state-yes a')->attr('href')
+        );
+        $this->assertStringContainsString(
+            '/album/virgin',
             (string) $crawler->filter('table#report tr.catch-state-total a')->attr('href')
         );
     }
