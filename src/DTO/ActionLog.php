eb006fc62211110fb7b25bd64ee55eec5a738b9b
Renaud Douze <renauddouze@gmail.com>
2023-09-20 11:07:16 +0200

    Add DTO to handle ActionLog with the new current/last (last was not used yet)

---
+++ src/DTO/ActionLog.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+class ActionLog
+{
+    /**
+     * @param int[] $details
+     */
+    private function __construct(
+        public readonly \DateTime $createdAt,
+        public readonly ?\DateTime $doneAt,
+        public readonly ?int $executionTime,
+        public readonly array $details,
+        public readonly ?string $errorTrace,
+    ) {
+    }
+
+    /**
+     * @param array<string|int|int[]> $data
+     */
+    public static function createFromArray(array $data): self
+    {
+        /** @var string */
+        $createdAt = $data['created_at'];
+        /** @var string */
+        $doneAt = $data['done_at'] ?? null;
+        /** @var int */
+        $executionTime = $data['execution_time'] ?? null;
+        /** @var int[] */
+        $details = $data['details'] ?? [];
+        /** @var string */
+        $errorTrace = $data['error_trace'] ?? null;
+
+        return new self(
+            new \DateTime($createdAt),
+            ($doneAt ? new \DateTime($doneAt) : null),
+            $executionTime,
+            $details,
+            $errorTrace,
+        );
+    }
+}
---
+++ tests/Unit/DTO/ActionLogTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\DTO;
+
+use App\DTO\ActionLog;
+use PHPUnit\Framework\TestCase;
+
+class ActionLogTest extends TestCase
+{
+    public function testFullCreateFromArray(): void
+    {
+        $actionLog = ActionLog::createFromArray([
+            'created_at' => '2023-03-21 08:34:47+00',
+            'done_at' => '2023-03-22 08:34:47+00',
+            'execution_time' => 12,
+            'details' => [
+                'truc' => 1,
+                'bidule' => 2,
+            ],
+            'error_trace' => 'Something bad happen',
+        ]);
+
+        $this->assertEquals(
+            new \DateTime('2023-03-21 08:34:47+00'),
+            $actionLog->createdAt,
+        );
+        $this->assertEquals(
+            new \DateTime('2023-03-22 08:34:47+00'),
+            $actionLog->doneAt,
+        );
+        $this->assertEquals(
+            12,
+            $actionLog->executionTime,
+        );
+        $this->assertEquals(
+            [
+                'truc' => 1,
+                'bidule' => 2,
+            ],
+            $actionLog->details,
+        );
+        $this->assertEquals(
+            'Something bad happen',
+            $actionLog->errorTrace,
+        );
+    }
+
+    public function testMinimalCreateFromArray(): void
+    {
+        $actionLog = ActionLog::createFromArray([
+            'created_at' => '2023-03-21 08:34:47+00',
+        ]);
+
+        $this->assertEquals(
+            new \DateTime('2023-03-21 08:34:47+00'),
+            $actionLog->createdAt,
+        );
+        $this->assertNull(
+            $actionLog->doneAt,
+        );
+        $this->assertNull(
+            $actionLog->executionTime,
+        );
+        $this->assertEmpty(
+            $actionLog->details,
+        );
+        $this->assertNull(
+            $actionLog->errorTrace,
+        );
+    }
+}
