3dab711bcb5a683b2ab793db6373962645ec15f1
Renaud Douze <renaud.douze@gmail.com>
2024-01-05 22:20:18 +0100

    Update vimeo/psalm (and fix new issue)

--- src/Security/UserProvider.php
+++ src/Security/UserProvider.php
@@ -10,6 +10,9 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 
+/**
+ * @implements UserProviderInterface<UserInterface>
+ */
 class UserProvider implements UserProviderInterface, PasswordUpgraderInterface
 {
     /**
