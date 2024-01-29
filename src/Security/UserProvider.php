bd3179834f64a97f388427074791c2c7ad2c48b0
Renaud Douze <renauddouze@gmail.com>
2024-01-29 08:56:12 +0100

    Simplify supportsClass conditions

--- src/Security/UserProvider.php
+++ src/Security/UserProvider.php
@@ -34,7 +34,7 @@ class UserProvider implements UserProviderInterface, PasswordUpgraderInterface
 
     public function supportsClass(string $class): bool
     {
-        return User::class === $class || is_subclass_of($class, User::class);
+        return User::class === $class;
     }
 
 
