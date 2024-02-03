2f5902f05c50273f6697ad575a6382aae164254b
Renaud Douze <renauddouze@gmail.com>
2024-02-03 18:23:30 +0100

    Use coverage with infection to avoid doing it twice

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -12,7 +12,7 @@ class AppExtension extends AbstractExtension
     public function getFilters(): array
     {
         return [
-            new TwigFilter('ksort', [$this, 'appKsort']),
+            new TwigFilter('ksort', [$this, 'ksort']),
         ];
     }
 
@@ -21,7 +21,7 @@ class AppExtension extends AbstractExtension
      *
      * @return mixed[]
      */
-    public function appKsort(array $array, int $flags = SORT_REGULAR): array
+    public function ksort(array $array, int $flags = SORT_REGULAR): array
     {
         ksort($array, $flags);
 
