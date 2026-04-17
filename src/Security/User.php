53d237097f48b63d81b5b82f3051d7f5ee8f4165
Renaud Douze <renaud.douze@gmail.com>
2026-04-17 08:00:02 +0200

    Remove PHPStan baseline

--- src/Security/User.php
+++ src/Security/User.php
@@ -55,6 +55,7 @@ class User implements UserInterface
     #[\Override]
     public function getUserIdentifier(): string
     {
+        /** @var non-empty-string */
         return $this->identifier;
     }
 
