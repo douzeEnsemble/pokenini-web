4e7bfa8f49b3d328fa5f9307bc1b4ffb4bf03067
Renaud Douze <renaud.douze@gmail.com>
2026-05-04 08:54:40 +0200

    Change logo link depends on is user is connected or not

--- tests/src/Integration/Controller/Common/FooterTest.php
+++ tests/src/Integration/Controller/Common/FooterTest.php
@@ -15,35 +15,70 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 #[CoversNothing]
 final class FooterTest extends WebTestCase
 {
-    use TestNavTrait;
+  use TestNavTrait;
 
-    public function testFooter(): void
-    {
-        $client = self::createClient();
+  public function testFooter(): void
+  {
+    $client = self::createClient();
 
-        $user = GetUserToken::getFakeUserToken('8764532', 'TestProvider');
-        $user->addTrainerRole();
-        $user->addAdminRole();
-        $client->loginUser($user, 'web');
+    $user = GetUserToken::getFakeUserToken('8764532', 'TestProvider');
+    $user->addTrainerRole();
+    $user->addAdminRole();
+    $client->loginUser($user, 'web');
 
-        $crawler = $client->request('GET', '/fr/album/dex');
+    $crawler = $client->request('GET', '/fr/album/dex');
 
-        $this->assertCountFilter($crawler, 1, 'footer');
+    $this->assertCountFilter($crawler, 1, 'footer');
 
-        $year = date('Y');
-        $this->assertStringContainsString("© 2022-{$year}", $crawler->filter('footer #copyright')->text());
-        $this->assertSame('Version 1.2.12', $crawler->filter('footer #copyright')->attr('title'));
+    $year = date('Y');
+    $this->assertStringContainsString("© 2022-{$year}", $crawler->filter('footer #copyright')->text());
+    $this->assertSame('Version 1.2.12', $crawler->filter('footer #copyright')->attr('title'));
 
-        $this->assertCountFilter($crawler, 1, 'footer ul');
-        $this->assertCountFilter($crawler, 4, 'footer ul li');
+    $this->assertCountFilter($crawler, 1, 'footer ul');
+    $this->assertCountFilter($crawler, 4, 'footer ul li');
 
-        $index = 0;
-        $this->assertStringContainsString('Accueil', $crawler->filter('footer ul li')->eq($index)->text());
-        ++$index;
-        $this->assertStringContainsString('Politique de confidentialité', $crawler->filter('footer ul li')->eq($index)->text());
-        ++$index;
-        $this->assertStringContainsString('Mentions Légales', $crawler->filter('footer ul li')->eq($index)->text());
-        ++$index;
-        $this->assertStringContainsString('Cookies', $crawler->filter('footer ul li')->eq($index)->text());
-    }
+    $index = 0;
+    $this->assertStringContainsString('Accueil', $crawler->filter('footer ul li')->eq($index)->text());
+    ++$index;
+    $this->assertStringContainsString('Politique de confidentialité', $crawler->filter('footer ul li')->eq($index)->text());
+    ++$index;
+    $this->assertStringContainsString('Mentions Légales', $crawler->filter('footer ul li')->eq($index)->text());
+    ++$index;
+    $this->assertStringContainsString('Cookies', $crawler->filter('footer ul li')->eq($index)->text());
+
+    $this->assertSame(
+      '/fr/album/dex',
+      $crawler->filter('.navbar-logo')->attr('href') ?? ''
+    );
+  }
+
+  public function testFooterAsGuest(): void
+  {
+    $client = self::createClient();
+
+    $crawler = $client->request('GET', '/fr');
+
+    $this->assertCountFilter($crawler, 1, 'footer');
+
+    $year = date('Y');
+    $this->assertStringContainsString("© 2022-{$year}", $crawler->filter('footer #copyright')->text());
+    $this->assertSame('Version 1.2.12', $crawler->filter('footer #copyright')->attr('title'));
+
+    $this->assertCountFilter($crawler, 1, 'footer ul');
+    $this->assertCountFilter($crawler, 4, 'footer ul li');
+
+    $index = 0;
+    $this->assertStringContainsString('Accueil', $crawler->filter('footer ul li')->eq($index)->text());
+    ++$index;
+    $this->assertStringContainsString('Politique de confidentialité', $crawler->filter('footer ul li')->eq($index)->text());
+    ++$index;
+    $this->assertStringContainsString('Mentions Légales', $crawler->filter('footer ul li')->eq($index)->text());
+    ++$index;
+    $this->assertStringContainsString('Cookies', $crawler->filter('footer ul li')->eq($index)->text());
+
+    $this->assertSame(
+      '/fr',
+      $crawler->filter('.navbar-logo')->attr('href') ?? ''
+    );
+  }
 }
