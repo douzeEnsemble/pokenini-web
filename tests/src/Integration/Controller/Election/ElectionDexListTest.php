aa180276edac0d08ff5addaf0e39fc8eae71003a
Renaud Douze <renaud.douze@gmail.com>
2026-05-07 22:53:47 +0200

    Refacto album/home and election dex items display

--- tests/src/Integration/Controller/Election/ElectionDexListTest.php
+++ tests/src/Integration/Controller/Election/ElectionDexListTest.php
@@ -45,16 +45,16 @@ final class ElectionDexListTest extends WebTestCase
         $this->assertSame('Choisir le dex pour lequel tu veux voter', $crawler->filter('h1')->text());
         $this->assertSame("Selon le dex, il y'a plus ou moins de pokémons, plus ou moins de formes. C'est à toi de voir", $crawler->filter('h2')->text());
 
-        $this->assertCountFilter($crawler, 21, '.election-dex-item');
-        $this->assertCountFilter($crawler, 21, '.election-dex-item .card-title');
-        $this->assertCountFilter($crawler, 21, '.election-dex-item .card-title a');
-        $this->assertCountFilter($crawler, 24, '.election-dex-item .badge');
-        $this->assertCountFilter($crawler, 21, '.election-dex-item p.small');
+        $this->assertCountFilter($crawler, 6, '.dex-item');
+        $this->assertCountFilter($crawler, 6, '.dex-item .card-title');
+        $this->assertCountFilter($crawler, 6, '.dex-item .card-title a');
+        $this->assertCountFilter($crawler, 9, '.dex-item .badge');
+        $this->assertCountFilter($crawler, 6, '.dex-item p.small');
 
-        $this->assertSame('71 Pokémons', $crawler->filter('.election-dex-item .badge')->eq(0)->text());
-        $this->assertSame('1 Pokémons', $crawler->filter('.election-dex-item .badge')->eq(1)->text());
+        $this->assertSame('832 Pokémons', $crawler->filter('.dex-item .badge')->eq(0)->text());
+        $this->assertSame('982 Pokémons', $crawler->filter('.dex-item .badge')->eq(1)->text());
 
-        $this->assertSame('/fr/election/redgreenblueyellow', $crawler->filter('.election-dex-item .card-title a')->eq(0)->attr('href'));
-        $this->assertSame('/fr/election/rubysapphireemerald', $crawler->filter('.election-dex-item .card-title a')->eq(2)->attr('href'));
+        $this->assertSame('/fr/election/swordshield', $crawler->filter('.dex-item .card-title a')->eq(0)->attr('href'));
+        $this->assertSame('/fr/election/homeshiny', $crawler->filter('.dex-item .card-title a')->eq(2)->attr('href'));
     }
 }
