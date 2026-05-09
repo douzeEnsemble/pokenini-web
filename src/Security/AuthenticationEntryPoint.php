2acaa5d0a3eb5b67514a2a30a532fe50ff6c53b9
Renaud Douze <renaud.douze@gmail.com>
2026-05-09 13:10:44 +0200

    Fix disconnected user under refresh token

--- src/Security/AuthenticationEntryPoint.php
+++ src/Security/AuthenticationEntryPoint.php
@@ -13,6 +13,11 @@ use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface
 
 final class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
 {
+    private const PROVIDER_ROUTES = [
+        'google' => 'app_connect_google_goto',
+        'discord' => 'app_connect_discord_goto',
+    ];
+
     public function __construct(
         private readonly RouterInterface $router,
     ) {}
@@ -26,6 +31,20 @@ final class AuthenticationEntryPoint implements AuthenticationEntryPointInterfac
     #[\Override]
     public function start(Request $request, ?AuthenticationException $authException = null): Response
     {
+        $session = $request->getSession();
+
+        /** @var null|string $provider */
+        $provider = $session->get('_security_provider');
+
+        if (is_string($provider) && array_key_exists($provider, self::PROVIDER_ROUTES)) {
+            $session->set('_security_target_path', $request->getUri());
+
+            return new RedirectResponse(
+                $this->router->generate(self::PROVIDER_ROUTES[$provider]),
+                Response::HTTP_TEMPORARY_REDIRECT
+            );
+        }
+
         return new RedirectResponse(
             $this->router->generate('app_home_index'),
             Response::HTTP_TEMPORARY_REDIRECT
--- tests/src/Unit/Security/AuthenticationEntryPointTest.php
+++ tests/src/Unit/Security/AuthenticationEntryPointTest.php
@@ -9,6 +9,8 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 use Symfony\Component\Routing\RouterInterface;
 
 /**
@@ -17,7 +19,7 @@ use Symfony\Component\Routing\RouterInterface;
 #[CoversClass(AuthenticationEntryPoint::class)]
 final class AuthenticationEntryPointTest extends TestCase
 {
-    public function testStart(): void
+    public function testStartWithNoProvider(): void
     {
         $router = $this->createMock(RouterInterface::class);
         $router
@@ -27,15 +29,73 @@ final class AuthenticationEntryPointTest extends TestCase
             ->willReturn('/home')
         ;
 
-        $authenticator = new AuthenticationEntryPoint(
-            $router,
-        );
+        $response = (new AuthenticationEntryPoint($router))->start($this->makeRequest());
 
-        $request = new Request();
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertSame('/home', $response->getTargetUrl());
+    }
 
-        /** @var RedirectResponse $response */
-        $response = $authenticator->start($request);
+    public function testStartWithGoogleProviderRedirectsToGoogleAndSavesTargetPath(): void
+    {
+        $router = $this->createMock(RouterInterface::class);
+        $router
+            ->expects($this->once())
+            ->method('generate')
+            ->with('app_connect_google_goto')
+            ->willReturn('/connect/g')
+        ;
 
-        $this->assertEquals('/home', $response->getTargetUrl());
+        $request = $this->makeRequest('google');
+        $response = (new AuthenticationEntryPoint($router))->start($request);
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertSame('/connect/g', $response->getTargetUrl());
+        $this->assertSame('http://localhost/fr/some-page', $request->getSession()->get('_security_target_path'));
+    }
+
+    public function testStartWithDiscordProviderRedirectsToDiscordAndSavesTargetPath(): void
+    {
+        $router = $this->createMock(RouterInterface::class);
+        $router
+            ->expects($this->once())
+            ->method('generate')
+            ->with('app_connect_discord_goto')
+            ->willReturn('/connect/dd')
+        ;
+
+        $request = $this->makeRequest('discord');
+        $response = (new AuthenticationEntryPoint($router))->start($request);
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertSame('/connect/dd', $response->getTargetUrl());
+        $this->assertSame('http://localhost/fr/some-page', $request->getSession()->get('_security_target_path'));
+    }
+
+    public function testStartWithUnknownProviderFallsBackToHome(): void
+    {
+        $router = $this->createMock(RouterInterface::class);
+        $router
+            ->expects($this->once())
+            ->method('generate')
+            ->with('app_home_index')
+            ->willReturn('/home')
+        ;
+
+        $response = (new AuthenticationEntryPoint($router))->start($this->makeRequest('unknown'));
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertSame('/home', $response->getTargetUrl());
+    }
+
+    private function makeRequest(?string $provider = null): Request
+    {
+        $session = new Session(new MockArraySessionStorage());
+        if (null !== $provider) {
+            $session->set('_security_provider', $provider);
+        }
+        $request = Request::create('http://localhost/fr/some-page');
+        $request->setSession($session);
+
+        return $request;
     }
 }
