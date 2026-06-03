01e42d7806c66bfea8706460af4c2ce20fde5f3b
Renaud Douze <renaud.douze@gmail.com>
2026-06-03 05:17:12 +0200

    Move chrome in docker compose from Dockerfile

--- tests/src/Browser/AbstractBrowserTestCase.php
+++ tests/src/Browser/AbstractBrowserTestCase.php
@@ -5,53 +5,34 @@ declare(strict_types=1);
 namespace App\Tests\Browser;
 
 use App\Security\User;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Symfony\Component\BrowserKit\Cookie;
 use Symfony\Component\Panther\Client;
 use Symfony\Component\Panther\PantherTestCase;
-use Symfony\Component\Security\Http\Authenticator\Token\PostAuthenticationToken;
 
 abstract class AbstractBrowserTestCase extends PantherTestCase
 {
     protected static function getNewClient(): Client
     {
-        $client = static::createPantherClient(
-            [
-                'browser' => static::CHROME,
-            ],
+        $capabilities = DesiredCapabilities::chrome();
+        $capabilities->setCapability('acceptInsecureCerts', true);
+
+        return static::createPantherClient(
+            ['browser' => static::SELENIUM],
             [],
             [
-                'capabilities' => [
-                    'acceptInsecureCerts' => true,
-                ],
-                'chromedriver_arguments' => [
-                    '--ignore-certificate-errors',
-                ],
-            ]
+                'host' => 'http://chrome:4444/wd/hub',
+                'capabilities' => $capabilities,
+            ],
         );
-
-        return $client;
     }
 
     protected function loginUser(Client $client, User $user): void
     {
-        $sessionFactory = $this->getContainer()->get('session.factory');
-        $session = $sessionFactory->createSession();
-
-        $firewallName = 'web';
-        $firewallContext = 'web';
-
-        $token = new PostAuthenticationToken($user, $firewallName, $user->getRoles());
-        $session->set('_security_'.$firewallContext, serialize($token));
-        $session->save();
+        $client->request('GET', '/fr/connect/f/c?t='.$user->getProfile());
 
         $cookieJar = $client->getCookieJar();
-
-        $client->request('GET', '/');
-
-        $sessionCookie = new Cookie($session->getName(), $session->getId(), null, null, '127.0.0.1', false, true);
-        $cookieJar->set($sessionCookie);
-
-        $trackerCookie = new Cookie('tarteaucitron', '!matomocloud=true', null, null, '127.0.0.1', false, false);
+        $trackerCookie = new Cookie('tarteaucitron', '!matomocloud=true', null, null, '', false, false);
         $cookieJar->set($trackerCookie);
     }
 }
