8c0eda4921cc0acc80ba8118eccd745c281e8bfd
Renaud Douze <renaud.douze@gmail.com>
2026-06-23 00:03:14 +0200

    Migrate action logs from keyed object to action_type array

--- src/Service/Back/GetActionLogsService.php
+++ src/Service/Back/GetActionLogsService.php
@@ -20,16 +20,17 @@ class GetActionLogsService extends AbstractBackService
             '/istration/action-logs'
         );
 
-        /** @var array<string, array{current: array{created_at: string, done_at: null|string, execution_time: null|int, details: array<string, int>, error_trace: null|string}, last: null|array{created_at: string, done_at: null|string, execution_time: null|int, details: array<string, int>, error_trace: null|string}}> */
+        /** @var array<int, array{action_type: string, current: array{created_at: string, done_at: null|string, execution_time: null|int, details: array<string, int>, error_trace: null|string}, last: null|array{created_at: string, done_at: null|string, execution_time: null|int, details: array<string, int>, error_trace: null|string}}> */
         $actionLogsData = JsonDecoder::decode($json);
 
         $list = [];
-        foreach ($actionLogsData as $item => $data) {
+        foreach ($actionLogsData as $data) {
+            $actionType = $data['action_type'];
             $currentData = $data['current'];
             $lastData = $data['last'];
 
-            $list[$item] = new ActionLogData(
-                $item,
+            $list[$actionType] = new ActionLogData(
+                $actionType,
                 $this->serializer->deserialize((string) json_encode($currentData), ActionLog::class, 'json'),
                 $lastData ? $this->serializer->deserialize((string) json_encode($lastData), ActionLog::class, 'json') : null,
             );
